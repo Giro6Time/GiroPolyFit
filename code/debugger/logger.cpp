426e0db9b5fb307a4113ddb92b6314940abdf6cd
Giro6Time <3139106326@qq.com>
2025-01-07 20:10:05 +0800

    feat: 添加fileLogger

--- code/debugger/logger.cpp
+++ code/debugger/logger.cpp
@@ -2,6 +2,7 @@
 
 #include "logger.h"
 #define FLUSH std::flush
+#define DEFAULT_FILE_NAME "GiroPolyfitLog"
 
 // LoggerStreamBUf
 
@@ -44,21 +45,98 @@ CoutLogger::CoutLogger()
 }
 
 CoutLogger::~CoutLogger()
-{
-    // if (Logger::instance())
-    // 	Logger::instance()->unregister_client(this);
+{ // 实现观察者模式
+    if (Logger::instance())
+        Logger::instance()->unregister_client(this);
 }
 
-void CoutLogger::out_message(const std::string &message) { std::cout << "INFO " << message << FLUSH; }
-void CoutLogger::warn_message(const std::string &message) { std::cout << "WARNING " << message << FLUSH; }
-void CoutLogger::error_message(const std::string &message) { std::cout << "ERROR " << message << FLUSH; }
+void CoutLogger::out_message(const std::string &message) { std::cout << "INFO: " << message << FLUSH; }
+void CoutLogger::warn_message(const std::string &message) { std::cout << "WARNING: " << message << FLUSH; }
+void CoutLogger::error_message(const std::string &message) { std::cout << "ERROR: " << message << FLUSH; }
 
+// FileLogger
+FileLogger::FileLogger() : log_file_(nil)
+{
+    std::string file_name = DEFAULT_FILE_NAME;
+    set_file_name(file_name);
+}
+
+FileLogger::FileLogger(const std::string &file_name) : log_file_(nil)
+{
+    set_file_name(file_name);
+}
+
+FileLogger::~FileLogger()
+{ // 实现观察者模式
+
+    Logger::instance()->unregister_client(this);
+    delete log_file_;
+    log_file_ = nil;
+}
+
+void FileLogger::out_message(const std::string &message)
+{
+    // 输出日志信息的实现
+    if (log_file_)
+    {
+        *log_file_ << "INFO: " << message << FLUSH;
+    }
+}
+
+void FileLogger::warn_message(const std::string &message)
+{
+    // 警告日志信息的实现
+    if (log_file_)
+    {
+        *log_file_ << "WARNING: " << message << FLUSH;
+    }
+}
+
+void FileLogger::error_message(const std::string &message)
+{
+    // 错误日志信息的实现
+    if (log_file_)
+    {
+        *log_file_ << "ERROR: " << message << FLUSH;
+    }
+}
+
+const std::string FileLogger::get_log_file_name()
+{
+    return log_file_name_;
+}
+
+void FileLogger::set_file_name(const std::string &value)
+{
+    // 设置文件名的实现
+    log_file_name_ = value;
+    if (log_file_)
+    {
+        delete log_file_;
+        log_file_ = nil;
+    }
+    if (log_file_name_.length() == 0)
+        log_file_name_ = DEFAULT_FILE_NAME;
+    log_file_ = new std::ofstream(log_file_name_);
+    if (!log_file_)
+    {
+        abort("Failed to open log file: " + log_file_name_);
+    }
+}
+
+// Logger
 #ifdef GIROPOLYFIT_EXPORTS
 Logger *Logger::instance_ = nil;
 #endif
 
 void Logger::initialize()
 {
+    std::lock_guard<std::mutex> lock(mutex_); // 预防一下多线程情况
+    if (instance_)
+    { // 防止重复初始化
+        Logger::warn() << "Logger already initialized. Reinitializing..." << std::endl;
+        delete instance_;
+    }
     instance_ = new Logger();
     Logger::out() << "Project initialized" << std::endl;
 }
@@ -78,7 +156,7 @@ void Logger::register_client(LoggerClient *c)
 
 void Logger::unregister_client(LoggerClient *c)
 {
-    clients.erase(c);
+    clients.erase(c); // 使用erase不会因为c被多次删除而发生崩溃
 }
 
 bool Logger::is_client(LoggerClient *c)
@@ -86,18 +164,41 @@ bool Logger::is_client(LoggerClient *c)
     return clients.find(c) != clients.end();
 }
 
+const std::string Logger::log_file_name()
+{
+    return file_client_->get_log_file_name();
+}
+
 Logger::Logger() : out_(this), warn_(this), error_(this)
 {
     // 初始化输出流并且设置默认输出client为coutLogger
     default_client_ = new CoutLogger();
     register_client(default_client_);
+    file_client_ = new FileLogger();
+    register_client(file_client_);
 }
 
 Logger::~Logger()
 {
     delete default_client_;
     default_client_ = nil;
+
+    if (file_client_)
+    {
+        delete file_client_;
+        file_client_ = nil;
+    }
+    // clients由set自动删除
 }
+
+bool Logger::rename_log_file(const std::string &file_name)
+{
+    file_client_ = new FileLogger(file_name);
+    register_client(file_client_);
+    return true;
+}
+
+
 
 LoggerStream &Logger::out()
 {
--- code/debugger/logger.h
+++ code/debugger/logger.h
@@ -3,9 +3,11 @@
 #include <sstream>
 #include <string>
 #include <set>
+#include <mutex>
 
 #include "../common.h"
 #include "../basic_types.h"
+#include "basic.h"
 
 #pragma once
 
@@ -60,6 +62,30 @@ public:
     void error_message(const std::string &message);
 };
 
+class FileLogger : public LoggerClient
+{
+public:
+    FileLogger();
+    FileLogger(const std::string &file_name);
+    ~FileLogger();
+
+    /// @brief 输出INFO 此处直接flush，如果需要换行，则使用Logger::out() << endl或者在value中直接添加换行符
+    void out_message(const std::string &message);
+    /// @brief 输出WARNING 此处直接flush，如果需要换行，则使用Logger::warn() << endl或者在value中直接添加换行符
+    void warn_message(const std::string &message);
+    /// @brief 输出ERROR 此处直接flush，如果需要换行，则使用Logger::error() << endl或者在value中直接添加换行符
+    void error_message(const std::string &message);
+    /// @return log的文件名
+    const std::string get_log_file_name() { return log_file_name_; }
+
+protected:
+    void set_file_name(const std::string &value);
+
+private:
+    std::string log_file_name_;
+    std::ostream *log_file_;
+};
+
 class GIROPOLYFIT_API Logger
 {
 public:
@@ -72,12 +98,28 @@ public:
     static LoggerStream &warn();
     static LoggerStream &error();
 
-    static Logger *instance() { return instance_; }
+    static Logger *instance()
+    {
+        if (!instance_) // singleton
+        {
+            // 预防一下多线程情况
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (!instance_)
+            { // Double-checked locking
+                initialize();
+            }
+        }
+        initialize();
+        return instance_;
+    }
 
     void register_client(LoggerClient *c);
     void unregister_client(LoggerClient *c);
     /// @brief 判断目标c是否已经注册
     bool is_client(LoggerClient *c);
+
+    const std::string log_file_name();
+    bool Logger::rename_log_file(const std::string& file_name);
 
 protected:
     // fulush stream
@@ -93,14 +135,16 @@ protected:
 
 private:
     static Logger *instance_;
+    static std::mutex mutex_;
 
     LoggerClient *default_client_;
+    FileLogger *file_client_; // FileClient不需要由外部对象来管理，因此直接由Logger管理。
 
     LoggerStream out_;
     LoggerStream warn_;
     LoggerStream error_;
 
-    std::set<LoggerClient *> clients; // 已注册的clients (观察者模式)
+    std::set<LoggerClient *> clients; // 已注册的clients (观察者模式)，当Logger的输出流收到消息时，将会通知所有的clients输出Log
 
     friend class LoggerStream;
 };
