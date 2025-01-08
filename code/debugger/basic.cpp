e66059d500df54b62cda2f97004457b05e7c6b61
Giro6Time <3139106326@qq.com>
2025-01-08 21:58:19 +0800

    feat: 添加了断言系列的方法

--- code/debugger/basic.cpp
+++ code/debugger/basic.cpp
@@ -10,11 +10,42 @@
 
 void abort(const std::string &message)
 {
-#ifdef WIN32
     if (message.length())
-        OutputDebugString(message.c_str());
+        Logger::error("Abort") << message << std::endl;
+#ifdef WIN32
     DebugBreak();
 #else
 
 #endif
+}
+
+void assertion_failed(
+    const std::string &condition_string,
+    const std::string &file, int line)
+{
+    Logger::error("Assert") << "Assertion failed: " << condition_string << std::endl;
+    Logger::error("Assert") << "File: " << file << std::endl;
+    Logger::error("Assert") << "Line: " << line << std::endl;
+    abort();
+}
+
+void range_assertion_failed(double value, double min, double max,
+                            const std::string &file, int line)
+{
+    Logger::error("Assert") << "Range assertion failed: "
+                            << value << " in "
+                            << "[ " << min << " ... " << max << " ]"
+                            << std::endl;
+    Logger::error("Assert") << "File: " << file << std::endl;
+    Logger::error("Assert") << "Line: " << line << std::endl;
+    abort();
+}
+
+void should_not_have_reached(
+    const std::string &file, int line)
+{
+    Logger::error("Assert") << "Control should not have reached this point:" << std::endl;
+    Logger::error("Assert") << "File: " << file << std::endl;
+    Logger::error("Assert") << "Line: " << line << std::endl;
+    abort();
 }
--- code/debugger/basic.h
+++ code/debugger/basic.h
@@ -1,4 +1,37 @@
 ﻿#pragma once
 #include <string>
 #include "common.h"
-void abort(const std::string &message = "");
+void abort(const std::string &message);
+
+// assertion
+void GIROPOLYFIT_API assertion_failed(
+    const std::string &condition_string,
+    const std::string &file, int line);
+
+void GIROPOLYFIT_API range_assertion_failed(double value, double min, double max,
+                                            const std::string &file, int line);
+
+void GIROPOLYFIT_API should_not_have_reached(
+    const std::string &file, int line);
+
+#define my_assert(x)                                    \
+    {                                                   \
+        if (!(x))                                       \
+        {                                               \
+            ::assertion_failed(#x, __FILE__, __LINE__); \
+        }                                               \
+    }
+
+#define my_range_assert(x, min_val, max_val)              \
+    {                                                     \
+        if (((x) < (min_val)) || ((x) > (max_val)))       \
+        {                                                 \
+            ::range_assertion_failed(x, min_val, max_val, \
+                                     __FILE__, __LINE__); \
+        }                                                 \
+    }
+
+#define my_assert_not_reached()                        \
+    {                                                  \
+        ::should_not_have_reached(__FILE__, __LINE__); \
+    }
