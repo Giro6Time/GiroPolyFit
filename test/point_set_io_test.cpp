a342211049ad9fb98fd56fd76674522acccc769f
Giro6Time <3139106326@qq.com>
2025-02-11 19:02:12 +0800

    feat: 添加gtest的框架，并且创建了pointset读入的测试（示例）

---
+++ test/point_set_io_test.cpp
@@ -0,0 +1,36 @@
+﻿
+#include "gtest/gtest.h"
+#include "point_set.h"
+#include "point_set_io.h"
+#include "logger.h"
+class PointSetIOTest : public ::testing::Test
+{
+protected:
+    PointSet *ps;
+
+    void SetUp() override
+    {
+        ps = nullptr;
+    }
+
+    void TearDown() override
+    {
+        if (!ps)
+            delete ps; // 在每次测试后释放 ps 指向的内存
+        ps = nullptr;
+    }
+};
+
+TEST_F(PointSetIOTest, ReadPointSet)
+{
+    ps = PointSetIO::read("D:/Study/Polyfit/GiroPolyFit/test/pointset.vg");
+    ASSERT_NE(ps, nullptr);
+}
+
+int main(int argc, char **argv)
+{
+    Logger::initialize();
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
